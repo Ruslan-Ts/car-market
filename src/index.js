e4ae95d11f8c90c1e661e575a521477fabcb2f98
Ruslan Tsuman <r.ts@me.com>
2023-12-03 22:59:57 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from 'redux-persist/integration/react';
 import { GlobalStyles } from './CommonStyle/CommonStyles.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
     <GlobalStyles />
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
-      <BrowserRouter basename="/car-market">
-        <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/car-market">
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </>
 );
