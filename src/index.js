20232ad9adc0882362c0a736c0f0d1f9228bd59b
Ruslan Tsuman <r.ts@me.com>
2024-02-28 09:25:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { GlobalStyles } from './CommonStyle/CommonStyles.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
